1d82e2e02a1727acc992eaf31cfd294895e7cf14
chaenabi <find1086@gmail.com>
2022-08-15 17:49:17 +0900

    Impl nav, banner

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
     api_key: 'e66b8bdb34833e7406396b9afcfbc05c',
-    language: 'ko-KR'
-  }
-})
+    language: 'ko-KR',
+  },
+});
 
-export default instance
+export default instance;
---
+++ src/components/Nav.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log("window scroll", window.scrollY);
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, [show]);
+
+  return (
+    <nav className={`nav ${show && "nav__black"} `}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      ></img>
+
+      <img
+        alt="User logged"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        className="nav__avatar"
+      ></img>
+    </nav>
+  );
+}
